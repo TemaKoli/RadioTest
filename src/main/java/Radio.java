79e188a8a0ea4a060810f004934161a353985514
Artemius <cpt.morgan0510@gmail.com>
2023-06-27 19:17:43 +0300

    Secomd commit

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -2,6 +2,21 @@ public class Radio {
     private int currentStation;
     private int currentVolume;
 
+    public void next() {
+        if (currentStation != 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+        public void prev() {
+            if (currentStation != 0) {
+                currentStation --;
+            } else {
+                currentStation = 9;
+            }
+        }
+
     public int getCurrentStation() {
         return currentStation;
     }
@@ -28,5 +43,16 @@ public class Radio {
             return;
         }
         this.currentVolume = currentVolume;
+    }
+
+    public void plusVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+    public void minusVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
     }
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -76,4 +76,124 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void StationNext(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+
+        radio.next();
+
+        int expected = 6;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationNextMoreMax(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.next();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationNextMax(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(8);
+
+        radio.next();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationNextMin(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.next();
+
+        int expected = 1;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationPrev(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+
+        radio.prev();
+
+        int expected = 4;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationPrevOnMin(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(1);
+
+        radio.prev();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationPrevMinToMax(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.prev();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void StationPrevFromMaximum(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.prev();
+
+        int expected = 8;
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void VolumeSelection() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(52);
+
+        int expected = 52;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void VolumeMoreMax(){
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(51);
+        radio.setCurrentVolume(101);
+
+        int expected = 51;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
